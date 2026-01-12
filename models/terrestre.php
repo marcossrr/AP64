0692a2e30433fd4dde78b62c98518e18b670841c
Marcos <mariro@alumnatflorida.es>
2026-01-12 10:47:02 +0100

    Creamos terrestre

---
+++ models/terrestre.php
@@ -0,0 +1,18 @@
+<?php
+
+class Terrestre extends Bestia{
+    private $habitat;
+
+    public function __construct($nombre, $especie, $p_lvl, $estado_salud, $habitat){
+        parent::__construct($nombre, $especie, $p_lvl, $estado_salud);
+        $this->habitat = $habitat;
+    }
+
+    public function getHabitat(){
+        return $this->habitat;
+    }
+
+    public function setHabitat(){
+        $this->habitat = $habitat;
+    }
+}
